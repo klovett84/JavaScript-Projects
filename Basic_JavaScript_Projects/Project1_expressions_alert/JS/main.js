7c0d358dd5b31c0f111f2ef3ea4112ff19b684e4
klovett84 <84836870+klovett84@users.noreply.github.com>
2021-06-03 17:17:05 -0500

    Added main.js and index.html to demonstrate basic JavaScript concepts.

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,30 @@
+// Print "Hello, world!" to document
+document.write("Hello, world!<br>");
+
+// Set string variable and create an alert
+var x = "This is a string.";
+window.alert(x);
+
+// Backslash (\) to allow quotation mark in string
+document.write('Here\'s a string with an apostrophe.<br>');
+
+// Plus sign to concatenate strings into one
+var a = "This is "
+var b = "a concatenated string.<br>";
+
+document.write(a+b)
+
+document.write(c);
+
+//Multiple variables declared in one statement
+var color = "blue", shape = "square", animal = "dog";
+
+//Colors of variable text changed
+var color = color.fontcolor("blue");
+var shape = shape.fontcolor("green");
+var animal = animal.fontcolor("orange");
+
+document.write(animal + "<br>");
+
+//Variable with an expression
+document.write(2+2)
